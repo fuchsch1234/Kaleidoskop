6851a3161bf02fad24717d0241ec651dfa9f3033
cf <abtjh5q@hmamail.com>
2019-05-16 07:36:43 +0200

    Dependencies and plugins for server project

--- Server/build.gradle.kts
+++ Server/build.gradle.kts
@@ -1,7 +1,35 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
+    val kotlinVersion = "1.3.31"
     kotlin("jvm")
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("org.springframework.boot") version "2.1.4.RELEASE"
+}
+
+apply(plugin="io.spring.dependency-management")
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    val junitVersion = "5.1.0"
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group="junit", module="junit")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.jsmart:zerocode-tdd:1.3.7")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junitVersion")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
@@ -10,4 +38,12 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
     }
     sourceCompatibility = "1.8"
+}
+
+tasks.withType<BootJar> {
+    mainClassName = "${group}.server.Server"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
