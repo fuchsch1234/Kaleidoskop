e0d3842788a508ec37f1c86a68a42c5ac51be3a9
cf <abtjh5q@hmamail.com>
2019-05-26 09:44:18 +0200

    Adds repository annotations to repository classes.

--- Server/src/main/kotlin/de/fuchsch/kaleidoskop/server/repositories/ImageRepository.kt
+++ Server/src/main/kotlin/de/fuchsch/kaleidoskop/server/repositories/ImageRepository.kt
@@ -2,7 +2,9 @@ package de.fuchsch.kaleidoskop.server.repositories
 
 import de.fuchsch.kaleidoskop.server.model.ImageDAO
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ImageRepository : JpaRepository<ImageDAO, Long> {
 
     fun findByTagsName(name: String): List<ImageDAO>
--- Server/src/main/kotlin/de/fuchsch/kaleidoskop/server/repositories/TagRepository.kt
+++ Server/src/main/kotlin/de/fuchsch/kaleidoskop/server/repositories/TagRepository.kt
@@ -2,8 +2,10 @@ package de.fuchsch.kaleidoskop.server.repositories
 
 import de.fuchsch.kaleidoskop.server.model.TagDAO
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface TagRepository : JpaRepository<TagDAO, Long> {
 
     fun findByName(name: String): Optional<TagDAO>
