56488e8a1a622f82166b715a9ddddd27861c5e90
cf <abtjh5q@hmamail.com>
2019-06-05 21:19:55 +0200

    Starts application maximized.

--- GUI/src/main/kotlin/de/fuchsch/kaleidoskop/gui/Kaleidoskop.kt
+++ GUI/src/main/kotlin/de/fuchsch/kaleidoskop/gui/Kaleidoskop.kt
@@ -1,6 +1,14 @@
 package de.fuchsch.kaleidoskop.gui
 
 import de.fuchsch.kaleidoskop.gui.views.MainView
+import javafx.stage.Stage
 import tornadofx.App
 
-class Kaleidoskop: App(MainView::class)
+class Kaleidoskop: App(MainView::class) {
+
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.isMaximized = true
+    }
+
+}
